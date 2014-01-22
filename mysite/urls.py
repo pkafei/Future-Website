90a15e8fb52f25b12aeb2c1a7ae5f53d1efd799a
Portia Burton <plburton@gmail.com>
2014-01-21 16:51:29 -0800

    Added another url path

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('', url(r'^$', TemplateView.as_view(template_name='index.html')),
@@ -10,4 +11,5 @@ urlpatterns = patterns('', url(r'^$', TemplateView.as_view(template_name='index.
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    ('^pages/', include('django.contrib.flatpages.urls')),
 )
